c86c6c2860ad17875be3f6c8f9d95dfa80a66f53
Sung Park <way11142001@gmail.com>
2023-04-27 11:30:30 -0500

    Got rid of timetsamp for now!

--- main.py
+++ main.py
@@ -7,35 +7,12 @@ from dateutil.relativedelta import relativedelta
 
 contents = main()
 #might be useful to destructure contents array to variables that have well-defined names so it's clear! 
-domain, earliestcreated, topic = contents 
-resp = requests.get(f"https://www.googleapis.com/customsearch/v1?key=AIzaSyBcD-oHPwnM6W7MpWSp2p1BHO_4ppkKUuE&cx=d44d7375edf8c41be&q={topic}&dateRestrict={'y[' + earliestcreated + ']'}&siteSearch={domain}")
+domain,topic = contents 
+resp = requests.get(f"https://www.googleapis.com/customsearch/v1?key=AIzaSyBcD-oHPwnM6W7MpWSp2p1BHO_4ppkKUuE&cx=d44d7375edf8c41be&q={topic}&siteSearch={domain}")
 body = resp.json()
 searchResults = body["items"]
 for item in body["items"]:
-    #get the last modified timestamp for each search item! 
-    if("pagemap" in item):
-        cur_pagemap = item["pagemap"]
-        if("metatags" in cur_pagemap):
-            metatags = cur_pagemap["metatags"]
-            cur_time = None 
-            #check if metatags have last_modified or date-modified timestamp! 
-            if("last_modified" in metatags):
-                cur_time = metatags["last_modified"]
-            if("date-modified" in metatags):
-                cur_time = metatags["date-modified"]
-            #if there is info on cur_time, check if it meets restriction on outdatedness! 
-            if cur_time: 
-                print("cur_time: ", cur_time)
-                if(relativedelta(datetime.now(), cur_time).years <= int(earliestcreated)):
-                    print("Good")
-                else:
-                    print("bad")
-            else:
-                print("No info on last updated timestamp!")
-        else:
-            print("No metatags!")
-    else:
-        print("No pagemap key found in current search item!")
+    print("item: ", item)
 
 
 
