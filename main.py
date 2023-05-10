e3efd93d8b1c865d8b606f2c7778264986e17cc2
Sung Park <way11142001@gmail.com>
2023-05-09 22:18:40 -0500

    Working on advanced Options

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import requests 
 import sys 
 from bs4 import BeautifulSoup
@@ -10,20 +11,47 @@ import openai
 import os 
 from decouple import config
 from flask import Flask, render_template, url_for, request, redirect
- 
-os.environ['REQUESTS_CA_BUNDLE'] = './root-ca.cer'
+cert_path = "./root-ca.cer"
+os.environ['REQUESTS_CA_BUNDLE'] = cert_path 
+#create soup instance!
+def tag_visible(element):
+    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+        return False
+    if isinstance(element, Comment):
+        return False
+    return True
 
-def generate_response(in_prompt):
+def text_from_html(body):
+    soup = BeautifulSoup(body, 'html.parser')
+    for a in soup.findAll('a', href=True):
+        a.extract()
+    texts = soup.findAll(text=True)
+    visible_texts = filter(tag_visible, texts)  
+    return " ".join(t.strip() for t in visible_texts)
+
+def generate_response(in_prompt, domain, earliest_date, latest_date):
     #read in env. variable values! 
     openai.api_key = config("openai.api_key")
     model_engine = config("model_engine") 
-    # contents = main()
-
+    
+    #If user don't provide domain, default to .com! 
+    if not domain:
+        domain = ".com"
+        
     #might be useful to destructure contents array to variables that have well-defined names so it's clear! 
     # domain,topic = contents 
-    domain=".org"
     topic=in_prompt
-    resp = requests.get(f"https://www.googleapis.com/customsearch/v1?key=AIzaSyBcD-oHPwnM6W7MpWSp2p1BHO_4ppkKUuE&cx=d44d7375edf8c41be&fields=items(link)&q={topic}&siteSearch={domain}", verify=False)
+    print("earliest_date_text: ", earliest_date)
+    print("latest_date_text: ", latest_date) 
+    resp = None 
+    if earliest_date and latest_date:
+        resp = requests.get(f"https://www.googleapis.com/customsearch/v1?key=AIzaSyBcD-oHPwnM6W7MpWSp2p1BHO_4ppkKUuE&cx=d44d7375edf8c41be&fields=items(link)&q={topic}&as_eq={'before:' + earliest_date + ' after:' + latest_date}&siteSearch={domain}", verify=False)
+    elif earliest_date: 
+        resp = requests.get(f"https://www.googleapis.com/customsearch/v1?key=AIzaSyBcD-oHPwnM6W7MpWSp2p1BHO_4ppkKUuE&cx=d44d7375edf8c41be&fields=items(link)&q={topic}&as_eq={'before:' + earliest_date}&siteSearch={domain}", verify=False)
+    elif latest_date:
+        resp = requests.get(f"https://www.googleapis.com/customsearch/v1?key=AIzaSyBcD-oHPwnM6W7MpWSp2p1BHO_4ppkKUuE&cx=d44d7375edf8c41be&fields=items(link)&q={topic}&as_eq={'after:' + latest_date}&siteSearch={domain}", verify=False)
+    else: 
+        resp = requests.get(f"https://www.googleapis.com/customsearch/v1?key=AIzaSyBcD-oHPwnM6W7MpWSp2p1BHO_4ppkKUuE&cx=d44d7375edf8c41be&fields=items(link)&q={topic}&siteSearch={domain}", verify=False)
     body = resp.json()
     searchResults = body["items"]
 
@@ -35,81 +63,53 @@ def generate_response(in_prompt):
     #for each link, resolve it to get web html doc and parse it using beautifulSoup module! 
     #for link in links: 
         #resolve link
-    link_resp = requests.get(links[0], verify=False)
-    html_content = link_resp.text
+    considered = set() 
+    length = len(links)
+    counter = 0 
+    links_list = []
+    while True:
+        cur = random.randint(0, length-1)
+        link = links[cur]
+        if counter == 3:
+            break 
+        if link not in considered: 
+            USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
+            headers = {"user-agent": USER_AGENT}
+            link_resp = requests.get(link, headers = headers)
+            html_content = link_resp.text
+            formatted= text_from_html(html_content)
+            formatted = formatted[:5000]
+            prompt = "The following text is all of the text on a website including things that are not important. Please summarize the most crucial and core parts of this content: "
+            prompt += formatted
+            response = openai.ChatCompletion.create(
+                model = model_engine,
+                messages = [
+                    {"role": "system", "content": "chat"},
+                    {"role": "user", "content": prompt},
+                ],
+                max_tokens = 2000
+            )
+            considered.add(link)
+            counter += 1 
+            links_list.append([link, response['choices'][0]['message']['content']])
 
-    #define parsing functions
-    # for a in soup.findAll('a', href=True):
-    #     a.extract()
-
-
-    #create soup instance!
-    def tag_visible(element):
-        if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
-            return False
-        if isinstance(element, Comment):
-            return False
-        return True
-
-    def text_from_html(body):
-        soup = BeautifulSoup(body, 'html.parser')
-        for a in soup.findAll('a', href=True):
-            a.extract()
-        texts = soup.findAll(text=True)
-        visible_texts = filter(tag_visible, texts)  
-        return " ".join(t.strip() for t in visible_texts)
-
-    formatted=text_from_html(html_content)
-    formatted = formatted[:5000]
-
-    # all_p_tags = soup.find_all("p")
-    # p_arr = [t.string for t in all_p_tags if type(t) is not None and type(t.string) is not None]
-    # p_arr = list(filter(lambda x: x != None, p_arr))
-
-    #generate prompt
-    # prompt="Please provide a summary on the following text: "
-    # overall_p = ""
-    # for e in p_arr:
-    #     overall_p += e
-    # prompt += overall_p[:50]
-    prompt = "The following text is all of the text on a website including things that are not important. Please summarize the most crucial and core parts of this content: "
-    prompt += formatted
-
-    #retrieve response
-    # response = openai.Completion.create(engine=model_engine, max_tokens=1000, prompt=prompt)
-    response = openai.ChatCompletion.create(
-        model = model_engine,
-        messages = [
-            {"role": "system", "content": "chat"},
-            {"role": "user", "content": prompt},
-        ],
-        max_tokens = 2000
-    )
-
-    #output response
-    return response['choices'][0]['message']['content']
-    # print("Here is the web info: ", p_arr)
+            print("earliest_date: ", earliest_date)
+            print("latest_date: ", latest_date)
+    return links_list
 
 app = Flask(__name__)
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
         task_content= request.form['content']
-        prompt_out = generate_response(task_content)
-        return render_template('index.html',fin_out=prompt_out)
+        domain = request.form['domain']
+        earliest_date = request.form['earliest_date_text'] 
+        latest_date = request.form['latest_date_text'] 
+        array = generate_response(task_content, domain, earliest_date, latest_date)
+        return render_template('index.html', link1 = array[0][0], link1_text=array[0][1], link2 = array[1][0], link2_text = array[1][1], 
+                               link3 = array[2][0], link3_text = array[2][1])
     else:
         return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
-
-
-
-
-
-
-
-
-
-    
+    app.run(port=5001, debug=True)
